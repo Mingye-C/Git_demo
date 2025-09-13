ac9d9eb9eedb3a625dd53fe165dd1b1be7e191f4
Mingye-C <mingye.c@outlook.com>
2025-09-13 22:44:20 +0800

    new

--- git_demo_py/判断素数.py
+++ git_demo_py/判断素数.py
@@ -1,14 +1,13 @@
-import sys
-a=int(input("输入一个数"))
-if a<=1:
-    print("无法判断")
-    sys.exit()
-flag=False
-for i in range(2,a):
-    if a%i==0:
-        flag=True
-        break
-if flag==True:
-    print("不是一个质数")
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):  # 只需要判断到 sqrt(n)
+        if n % i == 0:
+            return False
+    return True
+
+a = int(input("输入一个数: "))
+if is_prime(a):
+    print(a,"是一个质数")
 else:
-    print("是一个质数")
+    print(a,"不是一个质数")
---
+++ git_demo_py/求阶乘.py
@@ -0,0 +1,5 @@
+a=int(input("输入一个数"))
+result=1
+for i in range(2,a+1):
+    result=result*i
+print(result)
